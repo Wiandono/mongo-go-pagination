f344fb610be90fa4a4ca25fedc8129d4f4a7b8ae
Wian <wiandonosaputro@gmail.com>
2020-10-11 16:45:37 +0900

    Remove debug
    - Mark the first release of Wiandono/mongo-go-pagination

--- pagingQuery.go
+++ pagingQuery.go
@@ -6,7 +6,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"log"
 )
 
 // Error constants
@@ -204,9 +203,6 @@ func (paging *pagingQuery) Find() (findData *FindData, err error) {
 	if err != nil {
 		return nil, err
 	}
-
-	log.Println(cursor.Current)
-	log.Println(cursor.Current.String())
 
 	defer cursor.Close(context.Background())
 	var docs []bson.Raw
